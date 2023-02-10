7035520cc3bb42fbb27794a9302390c0f6115da2
trs714 <t.motoyama714@gmail.com>
2023-02-11 01:47:06 +0900

    build

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@
 module.exports = {
   siteMetadata: {
     title: `woodmask`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.woodmask.jp`
   },
-  plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
