1ad6da7f2610ecaccf38a32d48c75b1a8309ba91
trs714 <t.motoyama714@gmail.com>
2023-02-11 11:17:25 +0900

    rebuild

--- src/pages/404.js
+++ src/pages/404.js
@@ -30,14 +30,6 @@ const NotFoundPage = () => {
       <p style={paragraphStyles}>
         Sorry 😔, we couldn’t find what you were looking for.
         <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
         <Link to="/">Go home</Link>.
       </p>
     </main>
