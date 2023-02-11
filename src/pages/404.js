366fd19a7356755207318e44a2fe0ca203be4529
trs714 <t.motoyama714@gmail.com>
2023-02-11 11:05:21 +0900

    rebuild

--- src/pages/404.js
+++ src/pages/404.js
@@ -15,6 +15,13 @@ const headingStyles = {
 const paragraphStyles = {
   marginBottom: 48,
 }
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+}
 
 const NotFoundPage = () => {
   return (
@@ -22,6 +29,14 @@ const NotFoundPage = () => {
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
         Sorry 😔, we couldn’t find what you were looking for.
+        <br />
+        {process.env.NODE_ENV === "development" ? (
+          <>
+            <br />
+            Try creating a page in <code style={codeStyles}>src/pages/</code>.
+            <br />
+          </>
+        ) : null}
         <br />
         <Link to="/">Go home</Link>.
       </p>
